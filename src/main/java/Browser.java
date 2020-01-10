e9c25e4966c15d74c9686ff67ff8ebf90f8b4566
Edgar153 <xtheboneybrainx@gmail.com>
2020-01-10 15:54:35 -0800

    v1.0 Data Extraction Has Been Added

--- src/main/java/Browser.java
+++ src/main/java/Browser.java
@@ -1,14 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Browser implements BasicSiteFunctions
 {
 	private String url;
 	private WebDriver driver;
-	
-	public Browser(String url) 
+	public String[] sites;
+	private HashMap<String, String> webMap = new HashMap();
+	private static ChromeOptions options = new ChromeOptions();
+	public Browser(String url)
 	{
+		// The Following websites contain a products.json file, which allows for
+		// easier monitoring
+		sites = defaultSites();
+		WebDriverManager.chromedriver().version("79.0.3945.36").setup();
+		options.setExperimentalOption("excludeSwitches", new String[]
+		{"enable-automation"});
 		setUrl(url);
+	}
+	private String fetchJSONData()
+	{
+		String data = "";
+		try
+		{
+			String str;
+			URL url = new URL(this.url);
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			while (null != (str = br.readLine()))
+				data += str;
+		} catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return data;
+	}
+	public void initiateSearch() throws IOException
+	{
+		String JSON = fetchJSONData();
+		ObjectMapper mapper = new ObjectMapper();
+		// pretty print
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		JsonNode jsonObject = mapper.readTree(JSON);
+		//String formattedJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+		String Handles = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject.findValues("handle"));
+		BufferedWriter writer = new BufferedWriter(new FileWriter("json.txt"));
+		System.out.println(jsonObject.findValues("handle"));
+	    writer.write(Handles.replaceAll(", ", "\n"));
+	     
+	    writer.close();
 	}
 	public void setUrl(String url)
 	{
@@ -22,11 +75,22 @@ public class Browser implements BasicSiteFunctions
 
 	public void loadBrowser()
 	{
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 		driver.get(url);
 	}
 	public void closeBrowser()
 	{
 		driver.quit();
+	}
+	private String[] defaultSites()
+	{
+		return new String[]
+		{"https://kith.com/products.json",
+				"https://undefeated.com/products.json",
+				"https://shop.extrabutterny.com/products.json",
+				"https://bdgastore.com/products.json",
+				"https://cncpts.com/products.json",
+				"https://www.notre-shop.com/products.json",
+				"https://juicestore.com/products.json"};
 	}
 }
--- src/test/java/TestBrowser.java
+++ src/test/java/TestBrowser.java
@@ -1,22 +1,30 @@
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 public class TestBrowser
 {
+	String url = "https://kith.com/products.json";
+	Browser brows = new Browser(url);
 	@BeforeTest //annotation
-	public void initialBrowserTest()
+	public void initialUrlTest() throws InterruptedException
 	{
 		System.out.println("Initial Test Commencing: Opening Browswer and Loading Webpage...");
-		String url = "https://www.youtube.com/watch?v=Jdkrj2lDAEY";
-		Browser brows = new Browser(url);
 		//Assert that the URL was set.
 		Assert.assertEquals(brows.getUrl(), url);
-		
+		brows.loadBrowser();
 	}
 	@Test
-	public void sampleTest()
+	public void loadBrowserTest() throws IOException
 	{
 		System.out.println("Dummy Test");
+		brows.initiateSearch();
+		brows.closeBrowser();
 	}
 }
