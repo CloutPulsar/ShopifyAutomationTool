822cc87fe4889aec7ad96314d817ad6cb4e8b932
edgar153 <edgargranados153@gmail.com>
2020-07-01 22:26:39 -0700

    finish the login layout

--- src/main/java/com/ShopifyAIO/mainLayout/mainGui.java
+++ src/main/java/com/ShopifyAIO/mainLayout/mainGui.java
@@ -4,16 +4,13 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 public class mainGui extends Application 
 {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException 
 	{
-		
+		primaryStage.setTitle("TEST");
 		primaryStage.show();	
 	}
 
